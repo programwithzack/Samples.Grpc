222814aafe9d08302e291b8388e2b9d23490e1a1
Zack Lian <zlian@lifetime.com>
2022-06-15 20:19:56 -0600

    Add project files.

---
+++ Samples.Grpc.Api/Services/TodosService.cs
@@ -0,0 +1,48 @@
+﻿using Grpc.Core;
+using Bogus;
+
+namespace Samples.Grpc.Api.Services
+{
+    public class TodosService : Todos.TodosBase
+    {
+
+        private readonly static List<Todo> _todos;
+        private readonly static bool[] _todoCompleted = new[] { true, false };
+        static TodosService()
+        {
+            int id = 1;
+            var lorem = new Bogus.DataSets.Lorem("en");
+            var generator = new Faker<Todo>("en")
+            .RuleFor(o => o.UserId, f => f.Random.Number(0, 5))
+            .RuleFor(o => o.Id, f => id++)
+            .RuleFor(o => o.Title, f => lorem.Sentence())
+            .RuleFor(o => o.Completed, f => _todoCompleted[f.Random.Number(0, _todoCompleted.Length - 1)]);
+            _todos = Enumerable.Range(1, 99).Select(i => generator.Generate()).ToList();
+        }
+
+        private readonly ILogger<TodosService> _logger;
+        public TodosService(ILogger<TodosService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<Todo> GetTodo(GetTodoRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(_todos.Single(t => t.Id == request.Id));
+        }
+
+        public override Task<GetTodosResponse> GetTodos(GetTodosReqeust request, ServerCallContext context)
+        {
+
+            var result = new GetTodosResponse();
+            if (request.UserId > 0)
+                result.Todos.AddRange(_todos.Where(t => t.UserId == request.UserId));
+            else
+                result.Todos.AddRange(_todos);
+
+            return Task.FromResult(result);
+
+        }
+
+    }
+}
---
+++ Samples.Grpc.Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Net.Client;
+using Samples.Grpc.Api;
+
+Console.WriteLine("Getting todos...");
+Console.WriteLine();
+
+// Connect to the gRPC service and retrieve a list of todos.
+using var channel = GrpcChannel.ForAddress("http://localhost:5177");
+
+var client = new Todos.TodosClient(channel);
+var response = await client.GetTodosAsync(new GetTodosReqeust { UserId = 0 });
+
+// Display the number of todos found.
+var todos = response.Todos;
+string todoCountMessage = $"Found {todos.Count()} todos";
+
+Console.WriteLine(todoCountMessage);
+Console.WriteLine("".PadLeft(todoCountMessage.Length, '='));
+Console.WriteLine();
+
+foreach (var todo in response.Todos)
+{
+    Console.WriteLine($"Id: {todo.Id}, UserId: {todo.UserId}, Title: {todo.Title}, Completed: {todo.Completed}");
+}
+
+Console.WriteLine();
+Console.WriteLine("Press any key to exit");
+
+Console.Read();
